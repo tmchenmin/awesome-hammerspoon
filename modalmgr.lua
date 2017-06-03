f88cb41ba539ce8a4efff5803647e2ab208011bf
Jeoygin Wang <jeoygin@gmail.com>
2017-06-03 11:45:33 +0800

    Add key bindings for modalmgr

--- modalmgr.lua
+++ modalmgr.lua
@@ -24,6 +24,7 @@ function modalmgr:exited()
     end
 end
 
+modalmgr:bind("", "space", "Alfred 3", function() exit_others() activateApp("Alfred 3") end)
 modalmgr:bind("", "escape", "Exit Main Mode", function() modalmgr:exit() end)
 modalmgr:bind("", "Q", "Exit Main Mode", function() modalmgr:exit() end)
 
@@ -38,6 +39,7 @@ end
 if clipboardM then
     clipboardM_keys = clipboardM_keys or {"", "C"}
     if string.len(clipboardM_keys[2]) > 0 then
+        clipboardM:bind(modalmgr_keys[1], modalmgr_keys[2], "Enter Main Mode", function() exit_others() modalmgr:enter() end)
         modalmgr:bind(clipboardM_keys[1], clipboardM_keys[2], 'Enter Clipboard Mode', function() exit_others() clipboardM:enter() end)
     end
 end
@@ -79,6 +81,7 @@ end
 if cheatsheetM then
     cheatsheetM_keys = cheatsheetM_keys or {"", "S"}
     if string.len(cheatsheetM_keys[2]) > 0 then
+        cheatsheetM:bind(modalmgr_keys[1], modalmgr_keys[2], "Enter Main Mode", function() exit_others() modalmgr:enter() end)
         modalmgr:bind(cheatsheetM_keys[1], cheatsheetM_keys[2], 'Enter Cheatsheet Mode', function() exit_others() cheatsheetM:enter() end)
     end
 end
